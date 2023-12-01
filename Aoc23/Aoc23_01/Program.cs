ece7b81fd7ac4dc443ed67bb6c9dde20b0133fff
Fabian Fleischer <fabian.fleischer@crosberg.de>
2023-12-01 14:17:48 +0100

    Aoc23_01

--- Aoc23/Aoc23_01/Program.cs
+++ Aoc23/Aoc23_01/Program.cs
@@ -1,5 +1,34 @@
 ﻿internal class Program
 {
+	private static readonly Dictionary<string, int> numbers;
+
+	static Program()
+	{
+		Program.numbers = new Dictionary<string, int>()
+		{
+			{"0", 0},
+			{"1", 1},
+			{"2", 2},
+			{"3", 3},
+			{"4", 4},
+			{"5", 5},
+			{"6", 6},
+			{"7", 7},
+			{"8", 8},
+			{"9", 9},
+			{"zero", 0},
+			{"one", 1},
+			{"two", 2},
+			{"three", 3},
+			{"four", 4},
+			{"five", 5},
+			{"six", 6},
+			{"seven", 7},
+			{"eight", 8},
+			{"nine", 9}
+		};
+	}
+	
 	public static async Task Main(string[] args)
 	{
 		await Program.Part1();
@@ -26,9 +55,13 @@
 		int calibration = 0;
 		while (await reader.ReadLineAsync() is { } line)
 		{
-			char[] digits = line.Where(char.IsDigit).ToArray();
-			string number = $"{digits.First()}{digits.Last()}";
-			calibration += int.Parse(number);
+			string? keyFirst = Program.numbers.Keys.MinBy(k =>
+			{
+				int i = line.IndexOf(k, StringComparison.Ordinal);
+				return i < 0 ? int.MaxValue : i;
+			});
+			string? keyLast = Program.numbers.Keys.MaxBy(k => line.LastIndexOf(k, StringComparison.Ordinal));
+			calibration += Program.numbers[keyFirst] * 10 + Program.numbers[keyLast];
 		}
 
 		Console.WriteLine(calibration);
